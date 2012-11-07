ee3dcbdf008fe298ccf259b3fc50edb563bc4876
narudesign-sekig <makoto.skgc@gmail.com>
2012-11-07 23:52:23 +0900

    added admob implement project

---
+++ implementAdmob/implementAdmob/HelloWorldLayer.h
@@ -0,0 +1,25 @@
+//
+//  HelloWorldLayer.h
+//  implementAdmob
+//
+//  Created by 関口 誠 on 2012/11/07.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+#import "GADBannerView.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate, GADBannerViewDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
